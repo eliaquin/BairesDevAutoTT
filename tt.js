a00a37ec1a8b38ac3c90e4abcc179fdb0187668f
Eliaquin Encarnacion <eliaquin.diaz@bairesdev.com>
2020-09-12 19:04:35 -0400

    Fixed error in code

--- tt.js
+++ tt.js
@@ -21,7 +21,7 @@ async function work() {
         let buttonOut;
         for (let i = 0; i < workingDay.length; i++) {
             const work = workingDay[i];
-            const { hours } = work;
+            const { description, hours } = work;
             buttonIn = await driver.findElement(By.className('btn-blue'));
             buttonIn.click();
             await driver.wait(until.urlContains('Carga'));
@@ -31,7 +31,7 @@ async function work() {
                 await driver.findElement(By.id('ctl00_ContentPlaceHolder_idTipoAsignacionDropDownList')).sendKeys(asignamentFirstLetters, Key.RETURN);
             }, 1000);
             setTimeout(async () => {
-                await driver.findElement(By.id('ctl00_ContentPlaceHolder_DescripcionTextBox')).sendKeys(`${work.description} ${x + 1}`, Key.RETURN);
+                await driver.findElement(By.id('ctl00_ContentPlaceHolder_DescripcionTextBox')).sendKeys(description, Key.RETURN);
             }, 2000);
             setTimeout(async () => {
                 await driver.findElement(By.id('ctl00_ContentPlaceHolder_idFocalPointClientDropDownList')).sendKeys(focalPointFirstLetters, Key.RETURN);
